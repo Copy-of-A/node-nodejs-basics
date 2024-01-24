3171691da3aa693d2e9e04ad877ea3dd73f9e5e2
Alla Manshina <moralobsession@gmail.com>
2024-01-24 23:15:56 +0400

    feat: implement File system - rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,21 @@
+// @ts-check
+import * as fs from "node:fs/promises";
+import * as path from "node:path";
+import * as utils from "./utils.mjs";
+
 const rename = async () => {
-    // Write your code here 
+  const oldName = path.join(utils.DIRNAME, "files/wrongFilename.txt");
+  const newName = path.join(utils.DIRNAME, "files/properFilename.md");
+
+  const [isOldFileExist, isNewFileExist] = await Promise.all([
+    utils.ifPathExist(oldName),
+    utils.ifPathExist(newName),
+  ]);
+
+  if (!isOldFileExist || isNewFileExist)
+    throw new Error(utils.FS_ERROR_MESSAGE);
+
+  fs.rename(oldName, newName);
 };
 
 await rename();
