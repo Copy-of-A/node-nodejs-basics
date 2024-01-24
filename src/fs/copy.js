01e09e5fe28105063b8dea43dad1af4618bbb872
Alla Manshina <moralobsession@gmail.com>
2024-01-24 23:06:46 +0400

    feat: implement File system - copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,20 @@
+// @ts-check
+import * as fs from "node:fs/promises";
+import * as path from "node:path";
+import * as utils from "./utils.mjs";
+
 const copy = async () => {
-    // Write your code here 
+  const dirFrom = path.join(utils.DIRNAME, "files");
+  const dirTo = path.join(utils.DIRNAME, "files_copy");
+
+  const [isDirFromExist, isdirToExist] = await Promise.all([
+    utils.ifPathExist(dirFrom),
+    utils.ifPathExist(dirTo),
+  ]);
+
+  if (!isDirFromExist || isdirToExist) throw new Error(utils.FS_ERROR_MESSAGE);
+
+  await fs.cp(dirFrom, dirTo, { recursive: true });
 };
 
 await copy();
