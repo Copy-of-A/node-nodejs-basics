687f7648d73ab34098f5c7c4696d522c65a3fa71
Alla Manshina <moralobsession@gmail.com>
2024-01-26 22:19:13 +0400

    feat: implement Streams tasks

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,13 @@
+import { Transform } from "node:stream";
+
+const reverseStream = new Transform({
+  transform(chunk, _, callback) {
+    callback(null, String(chunk).split("").reverse().join(""));
+  },
+});
+
 const transform = async () => {
-    // Write your code here 
+  process.stdin.pipe(reverseStream).pipe(process.stdout);
 };
 
 await transform();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,17 @@
+import * as fs from "node:fs";
+import * as url from "node:url";
+import * as path from "node:path";
+
+export const DIRNAME = url.fileURLToPath(new URL(".", import.meta.url));
+
 const write = async () => {
-    // Write your code here 
+  const writableStream = fs.createWriteStream(
+    path.join(DIRNAME, "files/fileToWrite.txt"),
+    {
+      encoding: "utf-8",
+    }
+  );
+  process.stdin.pipe(writableStream);
 };
 
 await write();
