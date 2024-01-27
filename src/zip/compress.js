4b84ffcd731e3ac9f3e7044c26cedd8355410c7e
Alla Manshina <moralobsession@gmail.com>
2024-01-27 18:13:45 +0400

    feat: implement zlib tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,20 @@
+import * as zlib from "node:zlib";
+import * as stream from "node:stream/promises";
+import * as fs from "node:fs";
+import * as url from "node:url";
+import * as path from "node:path";
+
+const DIRNAME = url.fileURLToPath(new URL(".", import.meta.url));
+
 const compress = async () => {
-    // Write your code here 
+  const filepathSource = path.join(DIRNAME, "files", "fileToCompress.txt");
+  const filepathDest = path.join(DIRNAME, "files", "archive.gz");
+
+  const gzip = zlib.createGzip();
+  const source = fs.createReadStream(filepathSource);
+  const destination = fs.createWriteStream(filepathDest);
+
+  return stream.pipeline(source, gzip, destination);
 };
 
 await compress();
