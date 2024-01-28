d2ca52a0ff802ff2d2a8e42b7d7073e30dc6d8c0
Alla Manshina <moralobsession@gmail.com>
2024-01-29 00:49:07 +0400

    feat: implement child processes task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,17 @@
+//@ts-check
+import * as process from "node:child_process";
+import * as url from "node:url";
+import * as path from "node:path";
+
+export const DIRNAME = url.fileURLToPath(new URL(".", import.meta.url));
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const pathToChild = path.join(DIRNAME, "files", "script.js");
+
+  process.spawn("node", [pathToChild, ...args], {
+    stdio: ["inherit", "inherit", "inherit", "ipc"],
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["someArgument1", "someArgument2"]);
